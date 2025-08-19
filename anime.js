ddcbd2fc84e5530a9da5b59b84e1b64fd2a546d5
amirali chegeni <amiralichegeni@eduroam-193-157-185-119.wlan.uio.no>
2025-08-19 15:27:58 +0200

    Initial commit

---
+++ anime.js
@@ -0,0 +1,10 @@
+
+
+anime({
+    targets: '.block',
+    translateX: [0, 1300],   // flytter seg 250px til høyre
+    duration: 2500,    // 2 sekunder
+    easing: 'easeInOutQuad',
+    loop: true, //looper gjennom koden
+    borderRadius: ['0%', '50%'] //gjør den fra firekan til sirker
+  });
